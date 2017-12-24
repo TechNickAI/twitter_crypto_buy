94e2559fce538a9c4d6b3af6f500d79703430b36
Nick Sullivan <nick@sullivanflock.com>
2017-12-24 00:58:45 -0700

    Bot V1

--- README.md
+++ README.md
@@ -23,3 +23,6 @@ Originally built for the @officialmcaffee account after watching that whatever c
 1. Run `python bot.py` with the appropriate arguments (use `python bot.py --help` to see what they are)
 2. Profit (or loss)
 
+## TODO
+[ ] Sentiment analysis on the tweet to make sure that it is a positive mention
+[ ] Sanity checks to make sure we don't do stupid stuff like buy more than once
---
+++ bot.py
@@ -0,0 +1,88 @@
+from decimal import Decimal
+from twython import TwythonStreamer
+import requests
+import os
+import re
+
+settings = {
+    'twitter_userid': "12137672", # gorillamania (for testing)
+    #'twitter_userid': "961445378", # officialmcafee
+    'exchange_key': os.getenv("EXCHANGE_KEY"), # your Bittrex API key
+    'exchange_secret': os.getenv("EXCHANGE_SECRET"), # your Bittrex API secret
+    'buy_amount_btc': Decimal(0.75), # Amount to buy
+    'limit_multiplier': Decimal(1.05), # How much over the current ticker to set the limit order
+    'twitter_app_key': os.getenv("TWITTER_APP_KEY"),
+    'twitter_app_secret': os.getenv("TWITTER_APP_SECRET"),
+    'twitter_oauth_token': os.getenv("TWITTER_OAUTH_TOKEN"),
+    'twitter_oauth_secret': os.getenv("TWITTER_OAUTH_SECRET"),
+}
+
+"""
+Flow
+1. Set up the streamer to listen to all tweets from twitter_username
+2. When a tweet is found, parse for symbol
+3. Is it a valid symbol for Bittrex?
+4. BUY!
+"""
+
+def get_markets():
+    # Fetch the current markets so we know if it is a valid symbol
+    response = requests.get("https://bittrex.com/api/v1.1/public/getmarkets")
+    assert response.status_code == 200 
+    markets = []
+    for m in response.json()["result"]:
+        markets.append(m["MarketCurrency"])
+    return markets
+MARKETS = get_markets()
+
+
+def handle_tweet(tweet):
+    match = re.search("[A-Z]{3,5}", tweet)
+    if not match:
+        print("No symbol found in tweet: '%s'" % tweet)
+        return
+
+    symbol = match.group(0)
+    if symbol not in MARKETS:
+        print("%s is not tradeable on Bittrex" % symbol)
+        return
+
+    buy_crypto(symbol)
+
+
+def buy_crypto(symbol):
+    from bitex import Bittrex
+    e = Bittrex(settings["exchange_key"], settings["exchange_secret"])
+
+    pair = "BTC-" + symbol
+    print("Fetching current quote for %s" % pair)
+    quote = e.ticker(pair).formatted
+    current_price = Decimal(quote[6])
+
+    price = current_price * settings["limit_multiplier"]
+    amount = settings["buy_amount_btc"] / price
+    print("Buying %s of %s at %s" % (amount, symbol, price))
+    response = e.bid(pair, price, amount, False)
+    print(response.json())
+
+## Set up the streamer
+# https://twython.readthedocs.io/en/latest/usage/streaming_api.html
+class OurStreamer(TwythonStreamer):
+
+    def on_success(self, data):
+        print(data)
+        if 'text' in data:
+            handle_tweet(data["text"])
+
+    def on_error(self, status_code, data):
+        print("Error:", status_code, data)
+        self.disconnect()
+
+print("Setting up Twitter feed listener")
+stream = OurStreamer(
+        settings["twitter_app_key"],
+        settings["twitter_app_secret"],
+        settings["twitter_oauth_token"],
+        settings["twitter_oauth_secret"])
+stream.statuses.filter(follow=settings["twitter_userid"])
+print("Listening...")
